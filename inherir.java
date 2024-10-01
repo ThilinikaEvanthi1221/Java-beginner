29a81537d7184b16d9dc73ed6392467c3afa3c0b
Thilinika <evanthithilinika@gmail.com>
2024-10-01 21:10:06 +0530

    two

--- inherir.java
+++ inherir.java
@@ -1,6 +1,15 @@
 //inheritance - get one class properties to another class
+class Shape {
+    String color;
+}
+
+class Triangle extends Shape {
+
+}
+
 public class inherir {
     public static void main(String[] args) {
-
+        Triangle t1 = new Triangle();
+        t1.color = "red";
     }
 }
